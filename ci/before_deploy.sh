043892d832917b76c6b98acbf4ffe78ed419436b
Daichi Sakai <daisaru11@gmail.com>
2020-11-02 15:23:48 +0900

    Remove optimization option on release

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -17,7 +17,7 @@ main() {
 
     test -f Cargo.lock || cargo generate-lockfile
 
-    cross rustc --bin racco --target $TARGET --release -- -C lto
+    cross rustc --bin racco --target $TARGET --release
 
     cp target/$TARGET/release/racco $stage/
 
